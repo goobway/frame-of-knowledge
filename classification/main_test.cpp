aec8fa89ff630d5c6f34233253305abad5ad8dc7
goobway <calistagreenway@gmail.com>
2023-04-03 13:21:24 -0400

    more test

--- classification/main_test.cpp
+++ classification/main_test.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
 #include <vector>
 
-// Bilinear interpolation function
-float interpolate(float a, float b, float t) {
-    return a * (1 - t) + b * t;
+// Add this function to perform bilinear interpolation
+float bilinear_interpolate(const std::vector<std::vector<int>> &input, float x, float y)
+{
+    int x1 = (int)x;
+    int y1 = (int)y;
+    int x2 = x1 + 1;
+    int y2 = y1 + 1;
+
+    float r1 = (x2 - x) * input[y1][x1] + (x - x1) * input[y1][x2];
+    float r2 = (x2 - x) * input[y2][x1] + (x - x1) * input[y2][x2];
+
+    return (y2 - y) * r1 + (y - y1) * r2;
 }
 
-std::vector<std::vector<float>> resize(const std::vector<std::vector<int>>& input, int new_width, int new_height) {
-    int width = input.size();
-    int height = input[0].size();
+int main()
+{
+    // Your input code here
 
-    std::vector<std::vector<float>> output(new_height, std::vector<float>(new_width));
+    int oldWidth = 32;
+    int oldHeight = 32;
+    int newWidth = 28;
+    int newHeight = 28;
+    float scaleX = (float)oldWidth / (float)newWidth;
+    float scaleY = (float)oldHeight / (float)newHeight;
 
-    for (int y = 0; y < new_height; ++y) {
-        for (int x = 0; x < new_width; ++x) {
-            float gx = (float)(width - 1) * x / (new_width - 1);
-            float gy = (float)(height - 1) * y / (new_height - 1);
+    std::vector<std::vector<float>> output(newHeight, std::vector<float>(newWidth));
 
-            int gxi = (int)gx;
-            int gyi = (int)gy;
+    for (int i = 0; i < newHeight; i++)
+    {
+        for (int j = 0; j < newWidth; j++)
+        {
+            float srcX = j * scaleX;
+            float srcY = i * scaleY;
 
-            float dx = gx - gxi;
-            float dy = gy - gyi;
-
-            output[y][x] = interpolate(
-                interpolate(input[gyi][gxi], input[gyi][gxi + 1], dx),
-                interpolate(input[gyi + 1][gxi], input[gyi + 1][gxi + 1], dx),
-                dy
-            );
+            output[i][j] = bilinear_interpolate(input, srcX, srcY) / 255.0f;
         }
     }
 
-    return output;
-}
-
-int main() {
-    // Create a 32x32 vector of 1s and 0s
-    std::vector<std::vector<int>> input(32, std::vector<int>(32));
-    for (int i = 0; i < 32; ++i) {
-        for (int j = 0; j < 32; ++j) {
-            input[i][j] = (i + j) % 2;
-        }
-    }
-
-    // Resize the input vector to 28x28
-    std::vector<std::vector<float>> resized = resize(input, 28, 28);
-
-    // Print the resized vector
-    for (int i = 0; i < 28; ++i) {
-        for (int j = 0; j < 28; ++j) {
-            std::cout << resized[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // Your output code here
 
     return 0;
 }
