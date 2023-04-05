ce0a0b6b6aaf60be8ec7cab464aa2ba8f9d19fd0
goobway <calistagreenway@gmail.com>
2023-04-05 12:06:47 -0400

    comments

--- classification/main_test.cpp
+++ classification/main_test.cpp
@@ -18,3 +18,13 @@ cv::Mat preprocess(unsigned char input[32][32]) {
     // Return the float array
     return floatMat;
 }
+
+// sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+// git clone https://github.com/opencv/opencv.git
+// cd opencv
+// mkdir build
+// cd build
+// cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+// make -j$(nproc)
+// sudo make install
+// pkg-config --modversion opencv
