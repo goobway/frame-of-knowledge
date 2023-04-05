aaad1d7d8a02c3c802b148ab4cb97070acd0a8d5
goobway <calistagreenway@gmail.com>
2023-04-05 12:04:44 -0400

    main test using open cv

--- classification/main_test.cpp
+++ classification/main_test.cpp
@@ -1,80 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <opencv2/opencv.hpp>
 
-// Add this function to perform bilinear interpolation
-float bilinear_interpolate(const std::vector<std::vector<int>> &input, float x, float y)
-{
-    int x1 = (int)x;
-    int y1 = (int)y;
-    int x2 = x1 + 1;
-    int y2 = y1 + 1;
+cv::Mat preprocess(unsigned char input[32][32]) {
+    // Convert the input array to a cv::Mat
+    cv::Mat inputMat(32, 32, CV_8UC1, input);
 
-    float r1 = (x2 - x) * input[y1][x1] + (x - x1) * input[y1][x2];
-    float r2 = (x2 - x) * input[y2][x1] + (x - x1) * input[y2][x2];
+    // Resize the image to 28x28
+    cv::Mat resizedMat;
+    cv::resize(inputMat, resizedMat, cv::Size(28, 28));
 
-    return (y2 - y) * r1 + (y - y1) * r2;
-}
+    // Convert the resized image to a float array
+    cv::Mat floatMat;
+    resizedMat.convertTo(floatMat, CV_32FC1);
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+    // Normalize the pixel values to be between 0 and 1
+    floatMat /= 255.0;
 
-std::vector<std::vector<float>> normalize(const std::vector<std::vector<float>>& input) {
-    const int numRows = 28;
-    const int numCols = 28;
-
-    // Find the minimum and maximum values in the input vector
-    float minValue = input[0][0];
-    float maxValue = input[0][0];
-
-    for (int i = 0; i < numRows; ++i) {
-        for (int j = 0; j < numCols; ++j) {
-            minValue = std::min(minValue, input[i][j]);
-            maxValue = std::max(maxValue, input[i][j]);
-        }
-    }
-
-    // Calculate the range (max - min) of the input values
-    float range = maxValue - minValue;
-
-    // Create a new 2D vector to store the normalized values
-    std::vector<std::vector<float>> normalized(numRows, std::vector<float>(numCols));
-
-    // Normalize the input values by subtracting the minimum and dividing by the range
-    for (int i = 0; i < numRows; ++i) {
-        for (int j = 0; j < numCols; ++j) {
-            normalized[i][j] = (input[i][j] - minValue) / range;
-        }
-    }
-
-    return normalized;
-}
-
-int main()
-{
-    // Your input code here
-
-    int oldWidth = 32;
-    int oldHeight = 32;
-    int newWidth = 28;
-    int newHeight = 28;
-    float scaleX = (float)oldWidth / (float)newWidth;
-    float scaleY = (float)oldHeight / (float)newHeight;
-
-    std::vector<std::vector<float>> output(newHeight, std::vector<float>(newWidth));
-
-    for (int i = 0; i < newHeight; i++)
-    {
-        for (int j = 0; j < newWidth; j++)
-        {
-            float srcX = j * scaleX;
-            float srcY = i * scaleY;
-
-            output[i][j] = bilinear_interpolate(input, srcX, srcY) / 255.0f;
-        }
-    }
-
-    // Your output code here
-
-    return 0;
+    // Return the float array
+    return floatMat;
 }
